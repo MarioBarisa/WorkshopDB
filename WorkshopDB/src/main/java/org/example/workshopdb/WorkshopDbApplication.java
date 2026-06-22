dc8798fce1fce2e1264b929486884353059d4df7
Mario Barisa <mario@barisa.me>
2026-06-22 18:14:55 +0200

    Osnovni Spring projekt

---
+++ WorkshopDB/src/main/java/org/example/workshopdb/WorkshopDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.workshopdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopDbApplication.class, args);
+    }
+
+}
---
+++ WorkshopDB/src/test/java/org/example/workshopdb/WorkshopDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workshopdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
