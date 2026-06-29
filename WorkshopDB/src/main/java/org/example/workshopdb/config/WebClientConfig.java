59b3f7d173e7e7ba87df85b6e8963cc5ec07fbf1
Mario Barisa <mario@barisa.me>
2026-06-29 12:04:10 +0200

    Izmjena HTTP klijenta za slanje uptia na Openrouter, manje UI izmjene

--- WorkshopDB/src/main/java/org/example/workshopdb/config/WebClientConfig.java
+++ WorkshopDB/src/main/java/org/example/workshopdb/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package org.example.workshopdb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.JdkClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -14,7 +15,9 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+        return builder
+                .clientConnector(new JdkClientHttpConnector())
+                .build();
     }
 }
 
